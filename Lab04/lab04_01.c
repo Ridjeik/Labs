561a4fb1740880903ef407d8702914e826f4612c
Артем Марущак <artemmaruschak12@gmail.com>
2021-09-27 20:08:18 +0300

    First task initial commit

---
+++ Lab04/lab04_01.c
@@ -0,0 +1,68 @@
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_LIMIT 50
+int main()
+{
+    char sentence[MAX_LIMIT], keyword[MAX_LIMIT];
+
+    printf("Enter sentence: ");
+    fgets(sentence, MAX_LIMIT, stdin); 
+    sentence[strlen(sentence)-1] = '\0';
+
+    printf("Enter keyword: ");
+    fgets(keyword, MAX_LIMIT, stdin); 
+    keyword[strlen(keyword)-1] = '\0';
+
+    char key_chars[MAX_LIMIT] = { 0 };
+    int key_point = 0;
+
+    for(char* p = keyword; *p != '\0'; ++p){
+        if(strchr(key_chars, *p) == NULL){
+            key_chars[key_point] = *p;
+            ++key_point;
+        }
+    }
+
+
+    char* word = strtok(sentence, " ");
+
+    int match = 1;
+    while(word != NULL){
+
+        char word_chars[MAX_LIMIT] = { 0 };
+        int word_point = 0;
+
+        for(char* p = word; *p != '\0'; ++p){
+            if(strchr(word_chars, *p) == NULL){
+                word_chars[word_point] = *p;
+                ++word_point;
+            }
+        }
+
+
+        if(strlen(word_chars) == strlen(key_chars)){
+            match = 1;
+            for(char *c = key_chars; *c != '\0'; c++){
+                if(strchr(word_chars, *c) == NULL){
+                    match = 0;
+                    break;
+                }
+            }
+            if(match){
+                printf("Match! Word: %s", word);
+                break;
+            }
+            
+        }
+
+
+        word = strtok(NULL, " ");
+    }
+    if(!match){
+        printf("No match");
+    }
+    
+    return 0;
+
+}
