76133c4eeed7e0962d76d3ede108efed382d75f2
Артем Марущак <artemmaruschak12@gmail.com>
2021-10-16 21:36:44 +0300

    Added realiation of header file and driver code

---
+++ Lab05/lab05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lab05_func.h"
+
+int main(){
+    double a = 0, b = 0, h = 0;
+    printf("Enter a: ");
+    scanf("%lf", &a);
+    printf("Enter b: ");
+    scanf("%lf", &b);
+    printf("Enter step: ");
+    scanf("%lf", &h);
+    tabulate(a, b, h);
+}
